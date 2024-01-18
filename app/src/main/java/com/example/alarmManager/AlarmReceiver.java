77c6ae48f83c7a22e7dbb31f9243b3010d3f8c7b
SamirDiegoChavezCaceres <schavezca@unsa.edu.pe>
2024-01-18 15:16:05 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alarmManager/AlarmReceiver.java
@@ -0,0 +1,84 @@
+package com.example.alarmManager;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.BitmapFactory;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+
+public class AlarmReceiver extends BroadcastReceiver {
+    private NotificationManager notificationManager;
+    private PendingIntent pendingIntent;
+    private static int NOTIFICATION_ID = 1;
+    Notification notification;
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String NOTIFICATION_CHANNEL_ID = context.getString(R.string.app_name);
+        notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        Intent mIntent = new Intent(context, MainActivity.class);
+        Resources res = context.getResources();
+        Uri soundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM);
+
+        String message = context.getString(R.string.new_notification);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            final int NOTIFY_ID = 0;
+            String id = NOTIFICATION_CHANNEL_ID;
+            String title = NOTIFICATION_CHANNEL_ID;
+            PendingIntent pendingIntent;
+            NotificationCompat.Builder builder;
+            NotificationManager notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            if (notifManager == null) {
+                notifManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            }
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = notifManager.getNotificationChannel(id);
+            if (mChannel == null) {
+                mChannel = new NotificationChannel(id, title, importance);
+                mChannel.enableVibration(true);
+                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                notifManager.createNotificationChannel(mChannel);
+            }
+            builder = new NotificationCompat.Builder(context, id);
+            mIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(context, 0, mIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            builder.setContentTitle(context.getString(R.string.app_name)).setCategory(Notification.CATEGORY_SERVICE)
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setContentText(message)
+                    .setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.ic_notification))
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setSound(soundUri)
+
+                    .setContentIntent(pendingIntent)
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+            Notification notification = builder.build();
+            notifManager.notify(NOTIFY_ID, notification);
+
+
+        } else {
+            pendingIntent = PendingIntent.getActivity(context, 1, mIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            notification = new NotificationCompat.Builder(context)
+                    .setContentIntent(pendingIntent)
+                    .setSmallIcon(R.drawable.ic_notification)
+                    .setLargeIcon(BitmapFactory.decodeResource(res, R.drawable.ic_notification))
+                    .setSound(soundUri)
+                    .setAutoCancel(true)
+                    .setContentTitle(context.getString(R.string.app_name)).setCategory(Notification.CATEGORY_SERVICE)
+                    .setContentText(message).build();
+            notificationManager.notify(NOTIFICATION_ID, notification);
+        }
+        Log.d("IDNP", " ALARM RECEIVED!");
+    }
+}
